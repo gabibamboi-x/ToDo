45a42d8cac56e6b0bc1a07185b457aa201ad5bb4
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-11-04 18:16:50 +0100

    Call the initial render function and then for each new task

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import "./modules/modal"
 import "./modules/setComplete"
 import "./styles/main.css"
 import "./styles/modal.css"
-import "./modules/createToDo"
+import events from "./modules/events"
+import {render} from "./modules/render"
 import "./modules/displayController"
+import { allTasks } from "./modules/createNewToDo"
 
+// initial render
+allTasks.forEach(element => {
+  render(element)
+});
+
+events.on('newValidTask', (NewTodo) => {
+  allTasks.push(NewTodo)
+  render(NewTodo)
+  console.log(allTasks)
+})
