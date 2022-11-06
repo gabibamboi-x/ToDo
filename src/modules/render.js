57e6e1e1f1dbc06613efc87ef1b564ac8a02ae5a
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-11-06 01:55:59 +0100

    Modify the render function to support project tasks

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,15 +1,19 @@
 import { intlFormatDistance } from "date-fns"
-import {allTasks, createTaskNode } from "./createNewToDo"
+import { allTasks, createTaskNode } from "./createNewToDo"
+
+export const projectTasks = []
 
 export function render(el) {
-    // check for the date and if a task should be put in the today section or upcoming
-    const currentDistance = intlFormatDistance(new Date(el.dueDate), new Date, { unit: 'day' }).split(' ')
 
-    el.index = allTasks.indexOf(el)
-    el.overdue = false
+  // check for the date and if a task should be put in the today section or upcoming
+  const currentDistance = intlFormatDistance(new Date(el.dueDate), new Date, { unit: 'day' }).split(' ')
 
+  el.index = allTasks.indexOf(el)
+  el.overdue = false
+
+  if (el.projectName.toLowerCase() === 'inbox' ) {    
     if((currentDistance[0] === 'in' && currentDistance[1] > 0 && 
-    currentDistance[1] < 8 && currentDistance[2] === 'days') || currentDistance[0] === 'tomorrow') {
+        currentDistance[1] < 8 && currentDistance[2] === 'days') || currentDistance[0] === 'tomorrow') {
       document.querySelector('.upcomingContent').appendChild(createTaskNode(el))
     } else if (currentDistance[0] === 'yesterday' || currentDistance[2] === 'ago') {
       el.overdue = true
@@ -22,4 +26,10 @@ export function render(el) {
     }
 
     document.querySelector('.allContent').appendChild(createTaskNode(el))
+
+    return
+  }
+
+  document.querySelector('#' + el.projectName ).appendChild(createTaskNode(el))
+  projectTasks.push(el)
 };
