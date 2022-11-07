c3c02c2574d488f9ef477d9beb11b4f7c062c365
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-11-07 18:10:13 +0100

    Add support for localStorage and add unique ID's to the tasks

--- src/index.js
+++ src/index.js
@@ -4,29 +4,75 @@ import "./styles/projects.css"
 
 import "./modules/modal"
 import "./modules/setComplete"
+import "./modules/projects"
 import "./modules/displayController"
 import events from "./modules/events"
 import {render} from "./modules/render"
-import { allProjects } from "./modules/projects"
-import { allTasks } from "./modules/createNewToDo"
 import createTitleDOM from "./modules/createProject"
 
-// initial render of the normal tasks and projects
-allTasks.forEach(element => {
-  render(element)
-});
 
-allProjects.forEach(element => {
-  createTitleDOM(element)
-})
+
+
+// get the tasks & projects from the storage
+const getTasks = window.localStorage.getItem('storedTasks')
+const getProjects = window.localStorage.getItem('storedProjects')
+const storedTasks = JSON.parse(getTasks)
+const storedProjects = JSON.parse(getProjects)
+
+export const allProjects = []
+export const allTasks = []
+export let storageID = 141
+
+// if there are items stored render them
+if (storedProjects) {
+  storedProjects.forEach(element => {
+    createTitleDOM(element)
+    allProjects.push(element)
+  })
+}
+
+if (storedTasks) {
+  storedTasks.forEach(element => {
+    element.uniqueID = storageID
+    render(element)
+    allTasks.push(element)
+    storageID++
+  });
+}
+
+updateStorage()
 
 // update with each new addition of tasks and projects
 events.on('newProjectAdded', (p_title) => {
-  allProjects.push(p_title)
   createTitleDOM(p_title)
+
+  // confirm the new project to the user
+  document.querySelector('.feedback').innerText = 'Project added!'
+  setTimeout(function() {
+    document.querySelector('.feedback').innerText = ''
+  }, 2000)
+
+  updateStorage()
 })
 
+
 events.on('newValidTask', (NewTodo) => {
-  allTasks.push(NewTodo)
+  NewTodo.uniqueID = storageID
   render(NewTodo)
+  storageID++
+  
+  // confirm the new task to the user
+  document.querySelector('.feedback').innerText = 'Task added!'
+  setTimeout(function() {
+    document.querySelector('.feedback').innerText = ''
+  }, 2000)
+
+  updateStorage()
 })
+
+function updateStorage(){
+  // reset the storage and add the updated arrays
+  window.localStorage.clear()
+  window.localStorage.setItem('storedTasks', JSON.stringify(allTasks))
+  window.localStorage.setItem('storedProjects', JSON.stringify(allProjects))
+}
