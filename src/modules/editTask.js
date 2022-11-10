8603440cd4cff509b6111303d6e3b7698ba8a737
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-11-10 20:09:08 +0100

    Add the option to edit the Task

---
+++ src/modules/editTask.js
@@ -0,0 +1,45 @@
+import { allTasks, updateStorage } from "..";
+import { createTaskNode } from "./createNewToDo";
+import events from "./events";
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.edit').forEach(el => {
+    el.addEventListener('click', () => {
+
+      events.emit('taskID', Number(el.parentElement.parentElement.classList[0].replace('t', '')))
+      
+      document.querySelector('.confirm').textContent = 'Confirm'
+      
+      let currTask;
+      allTasks.forEach(task => {
+        if (task.uniqueID === Number(el.parentElement.parentElement.classList[0].replace('t', ''))) {
+          currTask = task
+        }
+      })
+
+      document.querySelector('.addTodo').click()
+      document.querySelector('#todotitle').value = currTask.title
+      document.querySelector('#todotitle').classList.add('ID' + currTask.uniqueID)
+      document.querySelector('#tododescription').value = currTask.description
+      document.querySelector('#dueDate').value = currTask.dueDate
+      document.querySelector('.location').setAttribute('selected', 'selected') 
+      document.querySelector('#' + currTask.priority).click()
+    })
+  })
+})
+
+events.on('taskEditConfirmed', (el) => {
+  allTasks.forEach(task => {
+    if (task.uniqueID === Number(el[1].replace('ID', ''))) {
+      task.title = el[0].title
+      task.description = el[0].description
+      task.dueDate = el[0].dueDate
+      task.priority = el[0].priority
+      task.projectName = el[0].projectName
+      task.uniqueID = Number(el[1].replace('ID', ''))
+      
+      updateStorage()
+
+      window.location.reload()
+  }})
+})
