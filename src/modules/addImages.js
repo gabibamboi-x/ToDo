0826b8574b7722def3bf992525aa7857e2d2bdd7
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-11-14 20:37:00 +0100

    Load the images through webpack

---
+++ src/modules/addImages.js
@@ -0,0 +1,21 @@
+import menu from "../Images/sort.png"
+import home from "../Images/home.png"
+import addTodo from "../Images/newToDo.png"
+import alltasks from "../Images/alltasks.svg"
+import today from "../Images/today.svg"
+import upcoming from "../Images/upcoming.svg"
+import important from "../Images/important.svg"
+import git from "../Images/gitlogo.png"
+import expand from "../Images/close_projects.svg"
+
+
+document.querySelector('.menu-nav').src = menu
+document.querySelector('.home').src = home
+document.querySelector('.addTodo').src = addTodo
+document.querySelector('.alltasks').src = alltasks
+document.querySelector('.today').src = today
+document.querySelector('.upcoming').src = upcoming
+document.querySelector('.important').src = important
+document.querySelector('.newProject').src = addTodo
+document.querySelector('.expand-projects').src = expand
+document.querySelector('.git').src = git
