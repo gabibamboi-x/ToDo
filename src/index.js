03c1b4c38e6c85e707f393beed38ea760ed1212f
gabibamboi-x <gabi.bamboi_x@icloud.com>
2022-11-15 20:24:37 +0100

    Remove the dispatchDOM and refactor the code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles/projects.css"
 import "./styles/checkbox.css"
 import "./styles/responsive.css"
 
+import "./modules/getDOM"
 import "./modules/modal"
 import "./modules/projects"
 import "./modules/deleteTasks&Projects"
@@ -11,16 +12,17 @@ import "./modules/editTask"
 import "./modules/displayController"
 import "./modules/createNewTask"
 import "./modules/addImages"
-import { currentScr } from "./modules/editTask"
 import events from "./modules/events.js"
 import {render} from "./modules/render.js"
-import { createAllDoneStatus } from "./modules/displayController"
+import { dom } from "./modules/getDOM"
+import { addMenuListener, createAllDoneStatus } from "./modules/displayController"
 import createTitleDOM from "./modules/createProject.js"
+import { deleteProject } from "./modules/deleteTasks&Projects"
 
 
+createAllDoneStatus(dom.allTasksTab)
 
 // get the tasks & projects from the storage
-const getPreviousScreen = JSON.parse(localStorage.getItem('scr'))
 const storedTasks = JSON.parse(localStorage.getItem('storedTasks'))
 const storedProjects = JSON.parse(localStorage.getItem('storedProjects'))
 
@@ -32,33 +34,34 @@ export let taskID = 141
 export let projectID = 141
 
 
-if (getPreviousScreen) {
-  document.querySelector('.' + getPreviousScreen).click()
-
-  if (!document.querySelector('.' + getPreviousScreen).firstChild) {
-    createAllDoneStatus(document.querySelector('.' + getPreviousScreen))
-  }
-} else {
-  createAllDoneStatus(document.querySelector('.allContent'))
-}
-
-
 // check if there are stored projects and/or tasks to avoid an error on the forEach loop if there aren't any
 if (storedProjects) {
   // if there are projects in the localStorage their elements will be created
   for (let i = 0; i < storedProjects.length; i++) {
     // give each project an unique id
     storedProjects[i].id = projectID
+
     
     // create the elements needed for the project
     createTitleDOM(storedProjects[i])
+
+    // add the delete event listener
+    const pDiv = document.querySelector('.p' + projectID)
+    addMenuListener(pDiv)
+  
+    const currentIcon = pDiv.querySelector('.binProject')
+    currentIcon.addEventListener( 'click' , function () {
+      deleteProject(currentIcon)
+    })
 
     // push them to the array that will be needed for updating the storage
     allProjects.push(storedProjects[i])
     projectID++
   }
-
 }
+
+
+
 
 if (storedTasks) {
   for (let i = 0; i < storedTasks.length; i++) {
@@ -79,22 +82,36 @@ if (storedTasks) {
   }
 }
 
+
+
+
 // update with each new addition of tasks and projects
 events.on('newProjectAdded', (project) => {
   // create the new project and increase the projectID
   createTitleDOM(project)
+
+  const pDiv = document.querySelector('.p' + projectID)
+  addMenuListener(pDiv)
+
+  const currentIcon = pDiv.querySelector('.binProject')
+  currentIcon.addEventListener( 'click' , function () {
+    deleteProject(currentIcon)
+  })
+
   projectID++
 
   // confirm the new project to the user
-  document.querySelector('.feedback').innerText = 'Project added!'
+  dom.feedback.innerText = 'Project added!'
   setTimeout(function() {
-    document.querySelector('.feedback').innerText = ''
+    dom.feedback.innerText = ''
   }, 2000)
 
   // dispatch DOM to bind the event listener to the new project selection
   updateStorage()
-  dispatchDOM()
 })
+
+
+
 
 events.on('newValidTask', (NewTodo) => {
   // assign an unique ID to the new task, see the rendering of storedTasks for details ↑
@@ -107,27 +124,26 @@ events.on('newValidTask', (NewTodo) => {
   updateStorage()
   
   // confirm the new task to the user for 2 seconds
-  document.querySelector('.feedback').innerText = 'Task added!'
+  dom.feedback.innerText = 'Task added!'
   setTimeout(function() {
-    document.querySelector('.feedback').innerText = ''
+    dom.feedback.innerText = ''
   }, 2000)
-
-  // dispatching the DOm again to bind the event listener to the new delete buttons
-  dispatchDOM()
 })
+
+
+
+// add the menu switching logic to the menu options
+dom.menu_items.forEach(item => {
+  addMenuListener(item)
+})
+
+
+
 
 export function updateStorage(){
   // reset the storage and add the updated arrays
   // much easier and less code than selecting individual items and update them
   window.localStorage.clear()
-  window.localStorage.setItem('scr', JSON.stringify(currentScr))
   window.localStorage.setItem('storedTasks', JSON.stringify(allTasks))
   window.localStorage.setItem('storedProjects', JSON.stringify(allProjects))
-}
-
-export function dispatchDOM(){
-  window.document.dispatchEvent(new Event("DOMContentLoaded", {
-    bubbles: true,
-    cancelable: true
-  }));
 }
